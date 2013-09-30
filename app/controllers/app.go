a0e8d0abda135439eba9718846e8582ef5ea8aa2
Tad DeVries <tad@splunk.net>
2013-09-29 20:30:26 -0600

    successfuly db connection

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,16 @@
 package controllers
 
-import "github.com/robfig/revel"
+import ( 
+	"github.com/robfig/revel"
+	"github.com/taddevries/lazyboy"
+)
 
 type App struct {
 	*revel.Controller
+	lazyboy.CouchDBController
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	temp := c.DBName
+	return c.Render(temp)
 }
